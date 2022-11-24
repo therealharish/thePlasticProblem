83095fce696b3ab01f41cee92abe52bb47bdae91
Harish <48938639+therealharish@users.noreply.github.com>
2022-11-25 02:06:51 +0530

    added menu

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
 import './App.css';
-import backgroundVideo from './components/videos/backgroundVid2.mp4'
-import facebook from './components/icons/facebook.png'
-import instagram from './components/icons/instagram.png'
-import twitter from './components/icons/twitter.png'
+import Home from './components/Home/Home.js'
 
 
 
 function App() {
   return (
     <div>
-      <section className = "showcase">
-        <header>
-          <div className = "logo">wellwecare</div>
-          <div className = "toggle"></div>
-        </header>
-        <video src = {backgroundVideo} type = "video/mp4" muted loop autoPlay></video>
-      
-        <div className = "overlay"></div>
-        <div className = "text">
-          <h2>The Plastic Problem</h2>
-          <h3>Tell leaders that a global treaty is needed to address the plastics crisis.</h3>
-          <a href = "https://www.plasticpollutioncoalition.org/" className = "btn">Learn More</a>
-        </div>
-
-        <ul class = "social">
-          <li>
-            <a href = "#">
-              <img src = {facebook} alt ="" width = "20" height = "20"/>
-            </a>
-          </li>
-          <li>
-            <a href = "#">
-              <img src = {instagram} alt =""width = "20" height = "20"/>
-            </a>
-          </li>
-          <li>
-            <a href = "#">
-             <img src = {twitter} alt =""width = "20" height = "20"/>
-            </a>
-          </li>
-        </ul>
-
-        <div className='Menu'>
-          <ul>
-            <li><a href = "#">Home</a></li>
-            <li><a href = "#">About</a></li>
-            <li><a href = "#">Contact</a></li>
-          </ul>
-        </div>
-      </section>
+      <Home />
 
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,63 @@
+import {useState} from 'react';
+import './Home.css';
+import backgroundVideo from '../videos/backgroundVid2.mp4'
+import facebook from '../icons/facebook.png'
+import instagram from '../icons/instagram.png'
+import twitter from '../icons/twitter.png'
+
+
+
+const Home = () => {
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  }
+
+  return (
+    <div>
+      <section className={!isActive ? "showcase": "showcase active"}>
+        <header>
+          <div className="logo">wellwecare</div>
+          <div className={!isActive ? "toggle": "toggle active"} onClick = {handleClick}></div>
+        </header>
+        <video src={backgroundVideo} type="video/mp4" muted loop autoPlay></video>
+
+        <div className="overlay"></div>
+        <div className="text">
+          <h2>The Plastic Problem</h2>
+          <h3>Tell leaders that a global treaty is needed to address the plastics crisis.</h3>
+          <a href="https://www.plasticpollutioncoalition.org/" className="btn">Learn More</a>
+        </div>
+
+        <ul class="social">
+          <li>
+            <a href="#">
+              <img src={facebook} alt="" width="20" height="20" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={instagram} alt="" width="20" height="20" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={twitter} alt="" width="20" height="20" />
+            </a>
+          </li>
+        </ul>
+      </section>
+      <div className='menu'>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
